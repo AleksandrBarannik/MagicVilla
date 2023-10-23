8cca3f0f11f87a4611cc7912a911311469290007
sniper1794 <sniper1794@mail.ru>
2023-10-23 23:52:46 +0300

    HttpPost CreatedVilla

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -7,7 +7,7 @@ namespace MagicVilla_VillaAPI.Controllers;
 //Устанавливаем маршрут
 [Route("api/VillaAPI")]
 [ApiController]
-public class VillaAPIController: ControllerBase
+public class VillaApiController: ControllerBase
 {
     //Задаем endPoint
     
@@ -18,7 +18,7 @@ public class VillaAPIController: ControllerBase
         return Ok(VillaStore.villaList);
     }
     
-    [HttpGet("{id:int}")]
+    [HttpGet("{id:int}",Name = "GetVilla")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound )]
@@ -35,6 +35,28 @@ public class VillaAPIController: ControllerBase
             return NotFound();
         }
         return Ok(villa);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError )]
+    public ActionResult<VillaDTO>CreateVilla([FromBody]VillaDTO? villaDto)
+    {
+        if (villaDto == null)
+        {
+            return BadRequest();
+        }
+
+        if (villaDto.Id > 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+        //Сортируем по убыванию и берем первый элемент
+        villaDto.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault()!.Id + 1;
+        VillaStore.villaList.Add(villaDto);
+        
+        return CreatedAtRoute("GetVilla",new {id = villaDto.Id},villaDto);
     }
     
 }
