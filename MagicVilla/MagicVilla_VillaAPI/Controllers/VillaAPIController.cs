af960ef86b5c3a16e2a2b523bde2fcb5bae38a16
sniper1794 <sniper1794@mail.ru>
2023-10-23 03:44:30 +0300

    Http MEtod Get: 1)set Route:api/VillaAPI 2)create REQUEST (GET)

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
 
+//Устанавливаем маршрут
+[Route("api/VillaAPI")]
 [ApiController]
 public class VillaAPIController: ControllerBase
 {
+    //Задаем endPoint
     
+    [HttpGet]
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa>
+        {
+            new Villa { Id = 1, Name = "Pool View" },
+            new Villa { Id = 2, Name = "Beach View" }
+        };
+    }
 }
