bc048e8bd35e364dcf6166b54e6c2caec122c7d7
sniper1794 <sniper1794@mail.ru>
2023-11-01 20:09:35 +0300

    Add MappingConfig (for AutoMapper)

---
+++ MagicVilla/MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models;
+namespace MagicVilla_Web;
+
+//Config Сопоставления Villa и VillaDTo между собой(в обе стороны)
+public class MappingConfig:Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<VillaDTO, Villa>().ReverseMap();
+        CreateMap<Villa, VillaCreateDTO>().ReverseMap();
+        CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+        
+        CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();;
+        CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
+        CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
+    }
+}
